636e114edcb158ca036be9b51ece4065da86f985
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-07-08 19:39:23 +0200

    estructura home

---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <div className="flex items-center justify-between   ">
+        <div className="flex flex-row  items-center  ">
+          <a href="/" className="text-3xl font-bold text-black align-middle">
+          <Image src="/logo.svg" alt="Robol Solutions" width={200} height={200}  />
+          </a>
+          <div className="hidden align-middle pl-14 xl:inline-block">
+            <a href="/" className="pr-12 text-xl text-black">
+              Home
+            </a>
+            <a href="/" className="pr-12 text-xl text-black">
+              Proyectos
+            </a>
+            <a href="/" className="pr-12 text-xl text-black">
+              Sobre nosotros
+            </a>
+            <a href="/" className="text-xl text-black">
+              Nuestros objetivos
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="hidden py-1 text-right xl:inline-block">
+            <a
+              className="inline-flex items-center px-12 py-3 mt-2 text-lg font-semibold tracking-tighter text-black"
+              href="/"
+            >
+              Log in
+            </a>
+            <a
+              className="inline-flex items-center px-8 py-3 mt-2 text-lg font-semibold tracking-tighter text-white bg-blue-800"
+              href="/"
+            >
+              Request a demo
+            </a>
+          </div>
+          <button className="pl-4 pr-12">
+            <svg
+              className="inline-block mr-auto text-black xl:hidden"
+              width="33"
+              height="50"
+              viewBox="0 0 23 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.892578 10.8691H22.1058"
+                stroke="black"
+                strokeLinecap="square"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M0.892578 18.8691H22.1058"
+                stroke="black"
+                strokeLinecap="square"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M22.1066 14.8688H0.893399"
+                stroke="black"
+                strokeLinecap="square"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    );
+}
